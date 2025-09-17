b0f509eef530620f83928c72524a96a19ca76b01
Nobu2025 <20243rd038@utez.edu.mx>
2025-09-17 16:18:48 -0600

    Archivo modificado practicaGithub: ahora es capaz de permitir mostrar repositorios encontrados

--- practicaGithub.py
+++ practicaGithub.py
@@ -8,3 +8,10 @@ git_dir = r"C:\Users\CC6\Desktop\practicasGit"
 repos = [name for name in os.listdir(git_dir)
          if os.path.isdir(os.path.join(git_dir,name)) and
          os.path.exists(os.path.join(git_dir, name, ".git"))]    
+if repos:
+    print("Found the following github repository")
+    for idx, repo in enumerate(repos, 1):
+        print(f"{idx},{repo}")
+else:
+    print("No github repositories found in C:\Users\CC6\Desktop\practicasGit")
+    print("Please provide a github repository")
