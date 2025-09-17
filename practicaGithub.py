93a853f46658a71c782c89f506f4502826475d6b
Nobu2025 <20243rd038@utez.edu.mx>
2025-09-17 16:23:48 -0600

    Archivo modificado: se corrigio error en el codigo faltando parte de C:\git

--- practicaGithub.py
+++ practicaGithub.py
@@ -4,14 +4,14 @@ import time
 import webbrowser
 #import request
 import winreg
-git_dir = r"C:\Users\CC6\Desktop\practicasGit"
+git_dir = r"C:\Users\CC6\Desktop\practica3"
 repos = [name for name in os.listdir(git_dir)
          if os.path.isdir(os.path.join(git_dir,name)) and
          os.path.exists(os.path.join(git_dir, name, ".git"))]    
 if repos:
-    print("Found the following github repository")
+    print("Found the following github repository in c:\\git.")
     for idx, repo in enumerate(repos, 1):
         print(f"{idx},{repo}")
 else:
-    print("No github repositories found in C:\Users\CC6\Desktop\practicasGit")
+    print("No github repositories found in C:\\git.")
     print("Please provide a github repository")
